011e650812d84349fa80254bf4b0d3fc28052247
quangminh0204 <minhtqhe141268@fpt.edu.vn>
2022-10-28 11:03:52 +0700

    first commit

---
+++ PRN211_E4_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRN211_E4_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PRN211_E4_MVC/Program.cs
@@ -0,0 +1,44 @@
+using PRN211_E4_MVC.Models;
+
+namespace PRN211_E4_MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<MusicStoreContext>();
+            // enable Session 
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession();
+            builder.Services.AddHttpContextAccessor();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // use Session
+            app.UseSession();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
